6b10eb0f0e8b4cbc7d3d1b8012a3123023be4485
Peter Reyes <codezilla@Peters-MBP.attlocal.net>
2021-04-19 20:15:46 -0500

    F21AS-10/Back-End-API-to-GET-list-of-all-warehouses/ added data folder and json files/ worked in warehouse.js file and server.js file /all warehouse data pulling on localhost8080/warehouse

--- server/routes/warehouse.js
+++ server/routes/warehouse.js
@@ -2,6 +2,39 @@ const express = require('express');
 // const fs = require('fs');
 // const uuid = require('uuid');
 const router = express.Router();
+const warehouseData = require('../data/warehouses.json');
+
+router.get('/', (_req, res)=> {
+    const Warehouses = warehouseData.map(warehouse => {
+        let singleWarehouse = {
+            id: warehouse.id,
+            name: warehouse.name,
+            address: warehouse.address,
+            city: warehouse.city,
+            country: warehouse.country,
+            contact: {
+            name: warehouse.name,
+            position: warehouse.position,
+            phone: warehouse.phone,
+            email: warehouse.email
+        } 
+    }
+        return singleWarehouse
+    })
+    res.status(201).send(Warehouses);     
+});  
+
+router.get('/:id', (req, res)=> {
+    let  everyWarehouse= req.params.id;
+    let result = warehouseData.find(result => result.id === everyWarehouse)
+    res.status(201).send(result);
+
+});
+
+// router.post('/', (req, res)=> {
+    
+// });
+
 
 
 module.exports = router
