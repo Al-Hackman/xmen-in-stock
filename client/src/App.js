445da4847553c7b8fac47a4f41cfaa991d7d6545
Albert Hackman <nana20p@yahoo.com>
2021-04-21 03:46:18 -0400

    Done with the Header

--- client/src/App.js
+++ client/src/App.js
@@ -3,11 +3,14 @@ import "./styles/global.scss";
 import { Switch, Route } from "react-router-dom";
 import WarehouseList from "./components/WarehouseList/WarehouseList";
 import WarehouseForm from "./components/WarehouseForm/WarehouseForm";
+import Header from "./components/Header/Header";
 
 class App extends React.Component {
     render() {
         return (
             <>
+                    <Header />
+
                 <Switch>
                     <Route
                         path="/"
