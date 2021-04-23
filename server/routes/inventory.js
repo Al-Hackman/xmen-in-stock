0027fbd88887e72182f48c6f344268533078bc4a
heather-ross <hross299@yahoo.com>
2021-04-23 01:09:53 -0400

    delete inventory item complete and tested with postman

--- server/routes/inventory.js
+++ server/routes/inventory.js
@@ -18,11 +18,28 @@ router.get("/", (_req, res) => {
     res.status(200).json(inventoryData);
 });
 
+//find inventory item
+router.get("/:id", (req, res) => {
+    let inventoryItem = inventoryData.find(
+        (result) => result.id === req.params.id
+    );
+    res.status(201).send(inventoryItem);
+});
+
 router.get("/warehouse/:id", (req, res) => {
     let warehouseInventories = inventoryData.filter(
-        (invetory) => invetory.warehouseID === req.params.id
+        (inventory) => inventory.warehouseID === req.params.id
     );
     res.status(200).json(warehouseInventories);
 });
+
+router.delete("/:id", (req, res) => {
+    const deleteInventoryItem = inventoryData.findIndex(
+        (item) => item.id === req.params.id);
+        inventoryData.splice(deleteInventoryItem, 1);
+        fs.writeFileSync("data/inventories.json", JSON.stringify(inventoryData));
+        res.json(inventoryData);
+})
+
 
 module.exports = router;
