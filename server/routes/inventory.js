db08d0e8b19d7867cbf67c9a89102f638d36dd8c
Peter Reyes <codezilla@Peters-MBP.attlocal.net>
2021-04-24 02:36:26 -0500

    F21AS push, tested and working on postman and new data sent to inventory json file

--- server/routes/inventory.js
+++ server/routes/inventory.js
@@ -58,4 +58,57 @@ router.delete("/:id", (req, res) => {
     }
 });
 
+
+router.put("/:id", (req, res) => {
+    // find warehouse
+    let inventoryItem = inventoryData.find(
+        (result) => result.id === req.params.id
+    );
+
+    // check for empty values inside object from request
+    let hasEmptyField = false;
+    Object.values(req.body).forEach((value) => {
+        if (value.length < 1) {
+            hasEmptyField = true;
+        }
+    });
+
+    // Regex to check phone number
+    // const phoneRegex = /^\+?(\d{1,2})?\s?\-?\.?\(?\d{3}[\-\)\.\s]?\s?\d{3}[\-\.\s]?\d{4}$/im;
+    // let isValidPhone = phoneRegex.test(req.body.phone);
+
+    // const emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    // let isValidEmail = emailRegex.test(req.body.email);
+
+    // Create new inventory object to add if only data is valid
+    if (!hasEmptyField) {
+        // let formattedPhoneNumber = formatter.formatPhone(req.body.phone);
+        inventoryItem.warehouseID = req.body.warehouseID;
+        inventoryItem.warehouseName = req.body.warehouseName;
+        inventoryItem.itemName = req.body.itemName;
+        inventoryItem.description = req.body.description;
+        inventoryItem.category = req.body.category;
+        inventoryItem.status = req.body.status;
+        inventoryItem.quantity = req.body.quantity;
+        
+
+        // write to file
+        try {
+            fs.writeFileSync(
+                "data/inventories.json",
+                JSON.stringify(inventoryData)
+            );
+        } catch (error) {
+            console.error("Error writing to inventories.json", error);
+        }
+
+        res.status(200).json(req.body);
+    } else {
+        res.status(500).send(
+            "New data is invalid."
+        );
+    }
+});
+
+
 module.exports = router;
